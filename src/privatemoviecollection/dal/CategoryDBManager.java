39ec893f607ae84e9f3ffbb35b008d4f787cb243
Péter Sebők <sebok.peter97@gmail.com>
2018-01-08 13:14:03 +0100

    Added the save() method to the CategoryDBManager

--- src/privatemoviecollection/dal/CategoryDBManager.java
+++ src/privatemoviecollection/dal/CategoryDBManager.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import privatemoviecollection.be.Category;
@@ -41,5 +42,34 @@ public class CategoryDBManager
             throw new DAException(ex);
         }
         return categories;
+    }
+    
+    /**
+     * Saves a category to the database
+     * @param cat The category that will be saved
+     * @throws DAException If an error occurs during database access
+     */
+    public void save(Category cat) throws DAException
+    {
+        try(Connection con = cm.getConnection())
+        {
+            PreparedStatement ps = con.prepareStatement("INSER INTO Category(id, name) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, cat.getId());
+            ps.setString(2, cat.getCategories());
+            int affected = ps.executeUpdate();
+            if (affected < 1)
+            {
+                throw new DAException("Category could not be saved");
+            }
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next())
+            {
+                cat.setId(rs.getInt("id"));
+            }
+        }
+        catch(SQLException ex)
+        {
+            throw new DAException(ex);
+        }
     }
 }
