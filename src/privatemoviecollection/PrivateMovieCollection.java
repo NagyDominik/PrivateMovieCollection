9b7facea57a0864a8aef50a5d6b8e1373376f97d
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-01-08 11:26:37 +0100

    BLLManager added

--- src/privatemoviecollection/PrivateMovieCollection.java
+++ src/privatemoviecollection/PrivateMovieCollection.java
@@ -24,6 +24,7 @@ public class PrivateMovieCollection extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setTitle("Private Movie Collection");
         stage.show();
     }
 
