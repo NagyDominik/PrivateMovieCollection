575cd67d648498bfbed43c565401c33dc45204e0
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-01-10 13:49:05 +0100

    improve inbuild

--- src/privatemoviecollection/gui/controller/PlayerController.java
+++ src/privatemoviecollection/gui/controller/PlayerController.java
@@ -9,6 +9,11 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,6 +22,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -24,7 +30,7 @@ import javafx.scene.media.MediaView;
  * @author Bence
  */
 public class PlayerController implements Initializable {
-
+    
     @FXML
     private Slider slider;
     @FXML
@@ -33,25 +39,57 @@ public class PlayerController implements Initializable {
     private MediaView mediaView;
     private MediaPlayer player;
     private Media media;
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        String path = new File("C:\\Users\\Bence\\Documents\\GitHub\\PrivateMovieCollection1\\Snowden.mp4").getAbsolutePath();
+        String path = new File("C:\\Users\\Bence\\Documents\\GitHub\\PrivateMovieCollection1\\testvideo1.mp4").getAbsolutePath();
         media = new Media(new File(path).toURI().toString());
         player = new MediaPlayer(media);
         mediaView.setMediaPlayer(player);
+        //changeListener();
+        valueChanger();
+        
+        slider.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                player.seek(Duration.seconds(slider.getValue()));
+            }
+        
+        });
+       
+        
+        
+        
     }
 
     //public void listener
     @FXML
     private void playClick(MouseEvent event) throws MalformedURLException {
         double d = media.getDuration().toSeconds();
-
         slider.setMax(d);
         System.out.println(d);
         player.play();
     }
-
+    
+ /*   public void changeListener() {
+        player.currentTimeProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                slider.setValue(player.getCurrentTime().toSeconds());
+            }
+        });
+    }*/
+    public void valueChanger(){
+        player.currentTimeProperty().addListener(new ChangeListener<Duration>(){
+            @Override
+            public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+               slider.setValue(newValue.toSeconds());
+            }
+        
+        
+        });
+    }
+    
     @FXML
     private void pauseClick(MouseEvent event) {
         String current = player.currentTimeProperty().toString();
@@ -59,4 +97,16 @@ public class PlayerController implements Initializable {
         System.out.println(media.durationProperty().toString());
         player.pause();
     }
+    
+    
+    @FXML
+    private void changedPosition(MouseEvent event) {
+        player.pause();
+        player.seek(Duration.seconds(slider.getValue()));
+        slider.setValue(player.currentTimeProperty().getValue().toMillis());// player.currentTimeProperty().getValue().toMillis()
+        
+        player.play();
+        
+    }
+    
 }
