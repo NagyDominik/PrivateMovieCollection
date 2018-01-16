075f994a37ab26d3224e576a6c3e3b893b89eac3
Péter Sebők <sebok.peter97@gmail.com>
2018-01-16 10:30:46 +0100

    Added comments and removed unnecessary methods from the BE package.

--- src/privatemoviecollection/be/Category.java
+++ src/privatemoviecollection/be/Category.java
@@ -1,18 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package privatemoviecollection.be;
 
 /**
- *
+ * A class that represent a category assigned to a movie. A movie can contain multiple categories.
  * @author Bence
  */
 public class Category {
 
-    private int id;
-    private String name;
+    private int id; //Unique identifier of the category. Used for storing/retrieving categories from the database.
+    private String name; //The name of the category (for example: action, drama, etc.)
 
     public Category() {
     }
@@ -22,10 +17,18 @@ public class Category {
         this.name = categories;
     }
 
+    /**
+     * Return the name of the category.
+     * @return The name of the category.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the name of the category to the specified value.
+     * @param name The name of the category that will be set.
+     */
     public void setName(String name) {
         this.name = name;
     }
